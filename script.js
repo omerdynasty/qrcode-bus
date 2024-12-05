8057640916ed66291181395fdf695773647356bf
Ömer <74985599+omerdynasty@users.noreply.github.com>
2024-12-05 07:12:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // script.js
 const textElement = document.getElementById('text');
 const cursor = document.getElementById('cursor');
-const clickContainer = document.getElementById('click-container');
 const redirectButton = document.getElementById('redirect-button');
 const text = "Hey nosy dude, looks like you scanned the QR code I (secretly) taped on the bus on your phone... Anyway, let's not drag things out any further... for you.";
 let index = 0;
@@ -12,17 +11,14 @@ function typeText() {
         index++;
 
         if (text[index - 1] === "." || text[index - 1] === "?") {
-            // Cümle sonlarında bekleme yap
-            setTimeout(typeText, 1000);
+            setTimeout(typeText, 1000); // Cümle sonlarında bekleme
         } else {
-            // Normal yazma
-            setTimeout(typeText, 75);
+            setTimeout(typeText, 75); // Normal yazma
         }
     } else {
         // Yazma tamamlandığında
-        cursor.classList.remove('blink'); // Cursor'un yanıp sönmesini durdur
-        cursor.style.display = "none"; // Cursor'u gizle
-        clickContainer.style.display = "block"; // "Click me" butonunu göster
+        cursor.style.display = "none"; // İmleci gizle
+        redirectButton.style.display = "block"; // Butonu göster
     }
 }
 
